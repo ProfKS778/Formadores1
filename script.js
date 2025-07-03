9b49e6a779dc22e86167d8873e3c5a525b27f548
ProfKS778 <kelly.teixeira.silva@escola.pr.gov.br>
2025-07-03 08:53:15 -0300

    auladia03.07

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function inserirNome() {
     elemento.textContent = nomeUsuario;
 }
 inserirNome();
+
+let linguagens = ["JavaScript", "Python", "C++"];
+console.log(linguagens[0]) //"JavaScript";
+console.log(linguagens[1]) //Python;
+console.log(linguagens[2]) //C++;
